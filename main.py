0e009a69fa0736fe7fca778075361d24352f320c
bhanurana430 <bhanurana430@gmail.com>
2025-02-03 19:52:40 +0100

    gg

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import yfinance as yf
 import os 
 
 st.image(os.path.join(os.getcwd(), 'static', 'bhanu-photo.jpeg'), width=200, caption='Bhanu Pratap Singh')
