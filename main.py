17d6a46ad12b2adbf406b932fe1fc533b70d8355
bhanurana430 <bhanurana430@gmail.com>
2025-02-03 19:52:17 +0100

    update

--- main.py
+++ main.py
@@ -18,28 +18,3 @@ p1 = st.write('Hello World')
 pressed = st.button('Button')
 
 st.divider()
-
-'This is a way to enter code nicely'
-
-code = '''
-# This is a code block
-def hello():
-    print("Hello, Streamlit!")
-'''
-st.code(code, language='python')
-
-st.divider()
-
-st.header('Dataframe')
-
-btc = yf.download(tickers='BTC-USD', period='1d', interval='1m')
-
-st.dataframe(btc)
-
-st.divider()
-
-'Editable dataframe'
-
-df = pd.DataFrame({'Aadi': [1, 2, 3], 'Bhanu': [4, 5, 6]})
-
-edit_df = st.data_editor(df)
